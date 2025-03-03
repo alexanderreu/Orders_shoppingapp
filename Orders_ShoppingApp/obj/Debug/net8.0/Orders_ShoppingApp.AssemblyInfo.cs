3718475587487727d8a682a930f874618171ef11
Alexander <alexanderreuter713@gmail.com>
2025-03-03 09:34:39 +0100

    Shopping schema lavet

--- Orders_ShoppingApp/obj/Debug/net8.0/Orders_ShoppingApp.AssemblyInfo.cs
+++ Orders_ShoppingApp/obj/Debug/net8.0/Orders_ShoppingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orders_ShoppingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee478329717a3d38f86a7b3e0954b28ea235f6bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3999c26bc93acd2514541b906cf9b46cb97e5a4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orders_ShoppingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orders_ShoppingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
